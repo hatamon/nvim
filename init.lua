db40022632a2dd55180dcd34125f034f7cc3a97a
hatamon <hatamontemp@gmail.com>
2024-06-09 17:42:17 +0900

    telescope追加

--- init.lua
+++ init.lua
@@ -196,3 +196,14 @@ require('lualine').setup()
 ---- indent-blankline / ui
 ----
 require("ibl").setup()
+---
+----
+---- telescope / search
+----
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+require('telescope').load_extension('project')
