ca469921306f9b2be7c04446c1636ea3a644d299
hatamon <hatamontemp@gmail.com>
2024-06-09 20:36:09 +0900

    いろいろ追加

--- init.lua
+++ init.lua
@@ -196,7 +196,7 @@ require('lualine').setup()
 ---- indent-blankline / ui
 ----
 require("ibl").setup()
----
+
 ----
 ---- telescope / search
 ----
@@ -207,3 +207,61 @@ vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
 require('telescope').load_extension('project')
+
+----
+---- toggleterm / ui
+----
+require'toggleterm'.setup({
+    open_mapping = '<C-\\>',
+    start_in_insert = true,
+    direction = 'float',
+})
+vim.cmd [[let &shell = '"C:/Program Files/Git/bin/bash.exe"']]
+vim.cmd [[let &shellcmdflag = '-s']]
+
+----
+---- gitsigns / git
+----
+require('gitsigns').setup {
+  signs = {
+    add          = { text = '┃' },
+    change       = { text = '┃' },
+    delete       = { text = '_' },
+    topdelete    = { text = '‾' },
+    changedelete = { text = '~' },
+    untracked    = { text = '┆' },
+  },
+  signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
+  numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
+  linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
+  word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
+  watch_gitdir = {
+    follow_files = true
+  },
+  auto_attach = true,
+  attach_to_untracked = false,
+  current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+  current_line_blame_opts = {
+    virt_text = true,
+    virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+    delay = 1000,
+    ignore_whitespace = false,
+    virt_text_priority = 100,
+  },
+  current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+  current_line_blame_formatter_opts = {
+    relative_time = false,
+  },
+  sign_priority = 6,
+  update_debounce = 100,
+  status_formatter = nil, -- Use default
+  max_file_length = 40000, -- Disable if file is longer than this (in lines)
+  preview_config = {
+    -- Options passed to nvim_open_win
+    border = 'single',
+    style = 'minimal',
+    relative = 'cursor',
+    row = 0,
+    col = 1
+  },
+}
