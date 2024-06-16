55c0d846d7561bdeff15f7cc7e5c5041c916da32
hatamon <hatamotemp@gmail.com>
2024-06-17 07:05:50 +0900

    Bizin Gothic NFに変更

--- init.lua
+++ init.lua
@@ -11,7 +11,7 @@ vim.keymap.set("t", "<C-V>p", function()
     return '<C-\\><C-n>"' .. next_char .. 'pi'
 end, { expr = true })
 
-vim.o.guifont = "Bizin Gothic:h12"
+vim.o.guifont = "Bizin Gothic NF:h12"
 vim.o.ambiwidth = 'double'
 
 ----
