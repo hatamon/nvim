af6757673e2fc9e2397b1b1280e04863a3e4db30
hatamon <hatamotemp@gmail.com>
2024-06-26 08:16:25 +0900

    telescopeのextension削除

--- init.lua
+++ init.lua
@@ -506,14 +506,11 @@ require("telescope").setup({
     },
 })
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", "<cmd>Telescope frecency workspace=CWD<CR>", {})
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.git_files, {})
 vim.keymap.set("n", "<leader>g", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
-
-require("telescope").load_extension("project")
-require("telescope").load_extension("frecency")
 
 ----
 ---- toggleterm / ui
