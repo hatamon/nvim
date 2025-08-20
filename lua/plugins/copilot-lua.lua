f66ebc8a7d6b4c3ee2927d42a1f4a392f29d29e6
mitsuhiro-hatano <hatamonroid@gmail.com>
2025-08-20 11:37:33 +0900

    copilot見直し

--- lua/plugins/copilot-lua.lua
+++ lua/plugins/copilot-lua.lua
@@ -2,11 +2,4 @@ return {
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",
     event = "InsertEnter",
-    config = function()
-        require("copilot").setup({
-            suggestion = { enabled = false },
-            panel = { enabled = false },
-            copilot_node_command = 'node'
-        })
-    end,
 }
